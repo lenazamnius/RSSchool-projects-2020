8f98132c1079967058fc2fc9612d1187c1048e51
lena zamnius <lena.zamnius@gmail.com>
2020-04-11 21:46:06 +0300

    feat: add animation to dropdown sidebar menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import cards from './cards-object';
 import './styles/main.scss';
 
-console.log(cards[1][1].word);
+const removeSidebar = document.getElementById('cross');
+const addSidebar = document.getElementById('toggle-sidebar');
+const sidebar = document.getElementById('sidebar-menu');
+
+addSidebar.addEventListener('click', () => {
+  sidebar.classList.add('active');
+});
+
+removeSidebar.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+});
