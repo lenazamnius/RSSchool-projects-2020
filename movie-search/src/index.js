509174163c9000b6a91ed83b4ac4061baf07eb5d
lena zamnius <lena.zamnius@gmail.com>
2020-04-26 22:44:01 +0300

    feat: add basic slider template

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,3 +1,17 @@
 import './styles/main.scss';
 
 console.log('Hello from main.js');
+
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
