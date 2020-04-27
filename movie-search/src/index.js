c4dd3fce9be117637a36eecdba3d01f483087d89
lena zamnius <lena.zamnius@gmail.com>
2020-04-27 17:21:20 +0300

    feat: add styles

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -1,17 +1,39 @@
 import './styles/main.scss';
 
-console.log('Hello from main.js');
+import MovieSlide from './movie-slide';
 
-const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 4,
-  spaceBetween: 10,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+
+function createSwiper(slidersAmount, space) {
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: slidersAmount,
+    spaceBetween: space,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  return swiper;
+}
+
+function swiperInitialization() {
+  const windowWidth = window.innerWidth;
+  
+  if(windowWidth < 630) {
+    createSwiper(1, 0)
+  } else if(windowWidth < 880) {
+    createSwiper(2, 10)
+  } else if(windowWidth < 1130) {
+    createSwiper(3, 10)
+  } else {
+    createSwiper(4, 15)
+  }
+}
+
+swiperInitialization();
+window.onresize = swiperInitialization;
---
+++ movie-search/src/movie-slide.js
@@ -0,0 +1,11 @@
+class MovieSlide {
+  constructor(name, image, year, rating, link) {
+    this.name = name;
+    this.image = image;
+    this.year = year;
+    this.rating = rating;
+    this.link = link;
+  }
+}
+
+export default MovieSlide;
