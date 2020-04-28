84fc6543be20c8622efa828000824dbe0a6e1daf
lena zamnius <lena.zamnius@gmail.com>
2020-04-28 19:19:15 +0300

    feat: add card generator class, fetch first data set from OMDB API

--- movie-search/src/index.js
+++ movie-search/src/index.js
@@ -2,8 +2,57 @@ import './styles/main.scss';
 
 import MovieSlide from './movie-slide';
 
+const inputString = 'cat';
+const swiperWrapper = document.getElementById('swiper-wrapper');
+const slides = document.querySelectorAll('.swiper-slide');
+const moviesData = [];
+
+function createNewSlide(id, title, poster, year, rating = 8) {
+  const newSlideObj = new MovieSlide(id, title, poster, year);
+  const newCardHtmlElement = newSlideObj.createSlideHtmlElements();
+
+  return newCardHtmlElement;
+  // swiperWrapper.appendChild(newCard);
+}
+
+const fetchData = async (inputStr) => {
+  const response = await fetch(`http://www.omdbapi.com/?apikey=3b910c7f&type=movie&s=${inputStr}`);
+  let data;
+  if (!response.ok) {
+    return;
+  } else {
+    data = await response.json();   
+  }
+  return data;
+}
+
+// fetchData(inputString);
+fetchData(inputString)
+.then(data => {
+  if (data.Response === 'False') {
+    console.log(`${inputString} not fond`);
+  } else {
+    for (let movie of data.Search) {
+      moviesData.push(movie);
+    }
+  }
+  return moviesData;
+})
+.then(moviesData => {
+  moviesData.forEach((movie) => {
+    const newCard = createNewSlide(movie.imdbID, movie.Title, movie.Poster, movie.Year);
+    swiperWrapper.appendChild(newCard);
+  })
+
+  swiperInitialization();
+})
+
+const inputWord = document.getElementById('inputted-word');
+inputWord.innerHTML = inputString;
+
 
 function createSwiper(slidersAmount, space) {
+  // console.log(slidersAmount, space)
   const swiper = new Swiper('.swiper-container', {
     slidesPerView: slidersAmount,
     spaceBetween: space,
@@ -35,5 +84,26 @@ function swiperInitialization() {
   }
 }
 
-swiperInitialization();
+// swiperInitialization();
 window.onresize = swiperInitialization;
+
+// fetch(`http://www.omdbapi.com/?apikey=3b910c7f&type=movie&s=${inputStr}`)
+  // .then((res) => {
+  //   if (!res.ok) {
+  //     return;
+  //   } else {
+  //     res.json().then((data) => {
+  //       if (data.Response === 'False') {
+  //         console.log(`${inputString} not fond`);
+  //       } else {
+  //         for (let movie of data.Search) {
+  //           console.log(movie);
+  //           moviesData.push(movie);
+  //         }
+  //       }       
+  //     });
+  //   }
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  // })
--- movie-search/src/movie-slide.js
+++ movie-search/src/movie-slide.js
@@ -1,11 +1,45 @@
 class MovieSlide {
-  constructor(name, image, year, rating, link) {
-    this.name = name;
-    this.image = image;
+  constructor(id, title, poster, year, rating) {
+    this.id = id;
+    this.title = title;
+    this.poster = poster;
     this.year = year;
     this.rating = rating;
-    this.link = link;
+  }
+
+  createSlideHtmlElements() {
+    const slideCard = document.createElement('div');
+    const movieTitle = document.createElement('a');
+    const moviePoster = document.createElement('div');
+    const movieYear = document.createElement('div');
+    const movieRating = document.createElement('div');
+    const star = this.rating < 7 ? '<i class="fas fa-star-half-alt"></i>' : '<i class="fas fa-star"></i>';
+
+    // add classes, attributes and values to elements
+    slideCard.classList.add('swiper-slide', 'd-flex', 'flex-column');
+    movieTitle.setAttribute('href', `https://www.imdb.com/title/${this.id}`);
+    movieTitle.setAttribute('target', `_blank`);
+    movieTitle.classList.add('title', 'p-3', 'text-muted', 'font-weight-bold');
+    movieTitle.innerHTML = this.title;
+    moviePoster.classList.add('card-body');
+    moviePoster.style.backgroundImage = `url(${this.poster})`;
+    movieYear.classList.add('year', 'p-3');
+    movieYear.innerHTML = this.year;
+    movieRating.classList.add('rating', 'px-3', 'pb-4', 'text-warning');
+    movieRating.setAttribute('data-id', this.id);
+    movieRating.innerHTML = star;
+
+    // create slide
+    slideCard.appendChild(movieTitle);
+    slideCard.appendChild(moviePoster);
+    slideCard.appendChild(movieYear);
+    slideCard.appendChild(movieRating);
+
+    return slideCard;
   }
 }
 
 export default MovieSlide;
+
+// http://www.omdbapi.com/?apikey=3b910c7f&i=tt3896198&
+// https://www.imdb.com/title/<imdbID'> to get a link to the movie
