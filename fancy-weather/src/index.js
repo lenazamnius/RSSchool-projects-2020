8f21ae31673500753964f8d267920e4fbe180bf6
lena zamnius <lena.zamnius@gmail.com>
2020-05-26 11:31:44 +0300

    feat: implement get and set current location

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -1,5 +1,7 @@
 import './assets/styles/main.scss';
 
+const { getName } = require('country-list');
+
 const apiKeys = {
   weather: 'qztMuAVTzLn0I1nWyyB7S0PMiGRt24ls',
   coordinates: '5e1ed30a88d24dd2a60ea5bfbb8bd5ec',
@@ -12,4 +14,42 @@ const store = {
   locationCountry: '',
 };
 
-console.log('hi there!')
+const weatherUrlValuesObj = {
+  time: 'daily',
+  endTime: '',
+  lat: '',
+  lon: '',
+  unit: 'si',
+  values: 'temp%2Cfeels_like%2Cwind_speed%2Chumidity%2Cweather_code',
+  apiKey: apiKeys.weather,
+};
+
+// get and set location
+async function getLocation() {
+  const response = await fetch('http://ipinfo.io/?token=f8a9f65202941f');
+  const location = await response.json();
+  const coordinatesArr = location.loc.split(',');
+
+  weatherUrlValuesObj.lat = coordinatesArr[0];
+  weatherUrlValuesObj.lon = coordinatesArr[1];
+  store.locationCity = location.city;
+  store.locationCountry = !store.locationCountry
+    ? getName(location.country)
+    : location.country;
+}
+
+function setLocation() {
+  const locationString = `${store.locationCity} ${store.locationCountry}`;
+
+  document.getElementById('latitude').innerHTML = weatherUrlValuesObj.lat;
+  document.getElementById('longitude').innerHTML = weatherUrlValuesObj.lon;
+  document.getElementById('location').innerHTML = locationString.toUpperCase();
+}
+
+async function setPage() {
+  store.locationCountry = '';
+  await getLocation();
+  await setLocation();
+}
+
+setPage();
