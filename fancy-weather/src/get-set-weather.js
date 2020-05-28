ce31d221c5e62204fdb19b5dd490c50aa53a4e46
lena zamnius <lena.zamnius@gmail.com>
2020-05-28 22:57:38 +0300

    refactor: change project structure

---
+++ fancy-weather/src/get-set-weather.js
@@ -0,0 +1,75 @@
+const curTemp = document.getElementById('temp');
+const weatherIcon = document.getElementById('weather-icon');
+const weatherType = document.getElementById('weather-type');
+const feelsTemp = document.getElementById('feels-like');
+const feelsTempUnit = document.getElementById('feels-unit');
+const windSpeed = document.getElementById('wind-speed');
+const windSpeedUnit = document.getElementById('wind-unit');
+const humidity = document.getElementById('humidity');
+const weekDay = document.querySelectorAll('.week-day');
+const futureTemps = document.querySelectorAll('.future-temp');
+const futureWeatherIcons = document.querySelectorAll('.future-weather-icon');
+const alertBox = document.getElementById('alert');
+const alertMessage = document.getElementById('error-message');
+
+async function getWeather(requestWeatherObj) {
+  const {
+    time,
+    endTime,
+    lat,
+    lon,
+    unit,
+    values,
+    apiKey,
+  } = requestWeatherObj;
+  const url = `https://api.climacell.co/v3/weather/forecast/${time}?lat=${lat}&lon=${lon}&unit_system=${unit}&start_time=now&end_time=${endTime}&fields=${values}&apikey=${apiKey}`;
+  let forecast;
+
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      forecast = await response.json();
+    } else {
+      const errorText = await response.json();
+      throw (errorText.message);
+    }
+  } catch (e) {
+    alertMessage.innerHTML = e;
+    alertBox.classList.toggle('hidden');
+  }
+
+  return forecast;
+}
+
+function setDataFromForecast(obj, index, lang, weatherDescriptionObj) {
+  const weatherCode = obj.weather_code.value;
+  const tempValue = Math.round(obj.temp[1].max.value);
+
+  if (index > 0) {
+    weekDay.forEach((val, idx) => {
+      const weekName = new Date(obj.observation_time.value).toLocaleString(lang, { weekday: 'long' });
+
+      if (index === idx + 1) val.innerHTML = weekName;
+    });
+    futureTemps.forEach((val, idx) => {
+      if (index === idx + 1) val.innerHTML = tempValue;
+    });
+    futureWeatherIcons.forEach((val, idx) => {
+      if (index === idx + 1) val.src = `../src/assets/images/${weatherCode}.svg`;
+    });
+  } else {
+    weatherType.innerHTML = weatherDescriptionObj[weatherCode];
+    curTemp.innerHTML = tempValue;
+    weatherIcon.src = `../src/assets/images/${weatherCode}.svg`;
+    feelsTemp.innerHTML = Math.round(obj.feels_like[0].min.value);
+    feelsTempUnit.innerHTML = obj.feels_like[1].max.units;
+    windSpeed.innerHTML = obj.wind_speed[1].max.value;
+    windSpeedUnit.innerHTML = obj.wind_speed[1].max.units;
+    humidity.innerHTML = obj.humidity[1].max.value;
+  }
+}
+
+export {
+  getWeather,
+  setDataFromForecast,
+};
---
+++ fancy-weather/src/time-date.js
@@ -0,0 +1,39 @@
+function setDateClock(lang, options) {
+  const now = new Date();
+  const dateString = new Date(now).toLocaleString(lang, options);
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+
+  if (hour.toString().length === 1) hour = `0${hour}`;
+  if (minute.toString().length === 1) minute = `0${minute}`;
+  if (second.toString().length === 1) second = `0${second}`;
+
+  const curDate = dateString.toUpperCase();
+  const curTime = `${hour}:${minute}:${second}`;
+  const dateClockString = `${curDate} <span class="clock">${curTime}</span>`;
+
+  return dateClockString;
+}
+
+// get forecast end date
+function endForecastDate(date, endDay) {
+  const copy = new Date(Number(date));
+
+  copy.setDate(date.getDate() + endDay);
+
+  return copy;
+}
+
+function endForecastDateIso(endDay) {
+  const todayDAte = new Date();
+  const endDate = endForecastDate(todayDAte, endDay);
+  const andDateIso = endDate.toISOString().split('T');
+
+  return andDateIso[0];
+}
+
+export {
+  setDateClock,
+  endForecastDateIso,
+};
